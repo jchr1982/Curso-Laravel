8019258f815e0d7d858b8e0cc4286a3965b6000f
jchr1982 <jchr1982@gmail.com>
2020-03-24 17:04:46 +0000

    Clase 9 - trabajo con rutas

---
+++ resources/views/permiso.blade.php
@@ -0,0 +1,8 @@
+Sistemas de permisos del sistema {{$nombre}}
+<br>
+
+<a href="{{url('sistema/admin/usuario/jchr')}}"> Dale aqui </a>
+<br>
+
+<a href="{{route('permiso5')}}"> Dale aqui tambien</a>
+
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,43 @@
 |
 */
 
+use Illuminate\Support\Facades\Route;
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Debes colocar la ruta, luego hacia donde voy a 
+// invocar y cual es el metodo que voy a llamar 
+// Ejemplo:
+// ruta: '/user'
+// hacia donde voy a invocar: 'UserController'
+// metodo a llamar: 'index'
+
+// Route::get('/user', 'UserController@index');
+
+// php artisan optimize
+// la optimizacion de rutas solo funciona con las
+// rutas directas
+
+Route::get('/permiso', 'PermisoController@index');
+
+Route::get('/permiso2', 'PermisoController@create');
+
+// Si solo quieres retornar una vista, colocar la ruta
+// y luego el nombre de la vista 'permiso.blade.php'
+Route::view('/permiso3', 'permiso');
+
+// con argumentos {argumento}
+Route::get('/permiso4/{nombre?}', 'PermisoController@index');
+
+// Rutas con nombres
+Route::get('sistema/admin/usuario/jchr', 'PermisoController@index')->name('permiso5');
+
+// usando expresion regular para validar el contenido
+// de la variable $nombre
+Route::get('permiso6/{nombre}', function ($nombre) {
+    //
+    return $nombre;
+})->where('nombre', '[A-Za-z]+')->name('permiso');
+
