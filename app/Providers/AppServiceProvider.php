3be5f4ef2f87cde312e02b8cde4945979ae87453
jchr1982 <jchr1982@gmail.com>
2020-04-03 14:01:17 +0100

    Menu Frintal - biblioteca - Video 27

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Models\Admin\Menu;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
@@ -14,7 +15,20 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        /*
+        Pasar una variable a una vista, en este caso 
+        la vista es aside.blade.php que esta en 
+        views/theme/lte, le paso la vista como tal
+        con $view, luego obtengo los menus (registros)
+        de la tabla menu del modelo Menu, y guardo esos
+        datos en la variable menusComposer.
+        */
+
+        View::composer("theme.lte.aside", function ($view) {
+            $menus = Menu::getMenu(true);
+            $view->with('menusComposer', $menus);
+        });
+        
         View::share('theme', 'lte');
 
     }
